d1a056a4d3392604581219139190594c4e348dd1
Ignacio Cinalli <nachocinalli@gmail.com>
2024-02-06 14:25:57 -0300

    fix header id

--- templates/slider-items.jsx
+++ templates/slider-items.jsx
@@ -1,26 +1,23 @@
 import React from 'react';
 import { templates, classes, html, compile } from 'core/js/reactHelpers';
 
-export default function slideritems({
-  onSliderChange,
-  _id,
-  _items,
-  _label,
-  ...props
-}) {
+export default function slideritems(props) {
+  const { onSliderChange, _id, _items, _label } = props;
   let _activeIndex = 0;
   const _max = _items.length - 1;
   const itemActive = props.model.getActiveItem();
   if (itemActive) {
     _activeIndex = itemActive.get('_index');
-  };
+  }
 
   return (
     <div className='component__inner slideritems__inner'>
       <templates.header {...props} />
       <div className='component__widget slideritems__widget'>
         <div className='slideritems__slider'>
-          <label htmlFor={`slider-input-${_id}`} className='slideritems__label'>{_label}</label>
+          <label htmlFor={`slider-input-${_id}`} className='slideritems__label'>
+            {_label}
+          </label>
           <input
             id={`slider-input-${_id}`}
             className='slideritems__slider-input'
@@ -32,48 +29,28 @@ export default function slideritems({
             aria-valuenow={_activeIndex}
             aria-valuetext={itemActive ? itemActive.get('title') : ''}
             onChange={onSliderChange}
-            list={`slider-input-data-${_id}`}>
-          </input>
+            list={`slider-input-data-${_id}`}
+          ></input>
           <datalist id={`slider-input-data-${_id}`}>
             {_items.map(({ _index, title }, index) => (
-              <option className={_activeIndex === _index ? 'is-active' : ''} key={index} value={_index}> {title}</option>
-
+              <option className={_activeIndex === _index ? 'is-active' : ''} key={index} value={_index}>
+                {' '}
+                {title}
+              </option>
             ))}
           </datalist>
         </div>
         <div className='slideritems__container-items' role='list'>
           {_items.map(({ title, body, _graphic, _isActive, _isVisited, _classes }, _index) => (
             <div
-              className={classes([
-                'slideritems__item',
-                _graphic.src && 'has-image',
-                _isActive && 'is-active',
-                _isVisited && 'is-visited',
-                _classes
-              ])}
+              className={classes(['slideritems__item', _graphic.src && 'has-image', _isActive && 'is-active', _isVisited && 'is-visited', _classes])}
               key={_index}
               data-index={_index}
-              role="listitem"
+              role='listitem'
             >
-
-              {title && (
-                <div className='slideritems__item-title'>
-                  {html(compile(title))}
-                </div>
-              )}
-              {body && (
-                <div className='slideritems__item-body'>
-                  {html(compile(body))}
-                </div>
-              )}
-              {
-                <templates.image {..._graphic}
-                  classNamePrefixes={[
-                    'component',
-                    'slideritems'
-                  ]}
-                />
-              }
+              {title && <div className='slideritems__item-title'>{html(compile(title))}</div>}
+              {body && <div className='slideritems__item-body'>{html(compile(body))}</div>}
+              {<templates.image {..._graphic} classNamePrefixes={['component', 'slideritems']} />}
             </div>
           ))}
         </div>
